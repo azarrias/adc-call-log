3cc0522595e6fe10c1634279ec083faa736d1e65
azarrias <adolfo.zarrias@gmail.com>
2018-11-08 21:07:17 +0100

    Handle call events

--- app/src/main/java/es/pue/android/calllog/CallReceiver.java
+++ app/src/main/java/es/pue/android/calllog/CallReceiver.java
@@ -3,6 +3,9 @@ package es.pue.android.calllog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.telecom.TelecomManager;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 
 public class CallReceiver extends BroadcastReceiver {
@@ -10,5 +13,13 @@ public class CallReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.i("RECEIVER", "Call event");
+        Bundle data = intent.getExtras();
+        if (data != null) {
+            String state = data.getString(TelephonyManager.EXTRA_STATE);
+            if (state.equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                String phoneNumber = data.getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
+                Log.i("INCOMING_NUMBER", phoneNumber);
+            }
+        }
     }
 }
